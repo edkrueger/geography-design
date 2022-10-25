2b9cbcdb62cde898cc0509c04996a38013947d30
edkrueger <edkrueger@gmail.com>
2022-10-25 00:39:01 +0000

    Use min distance instead of mean distance for design matrix.

--- design.py
+++ design.py
@@ -1,9 +1,13 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv("data/distance.csv")
 
 agg_df = df.pivot_table(
-    index=["poll_id", "poll_lat", "poll_lon"], columns="aid_id", values="haversine"
+    index=["poll_id", "poll_lat", "poll_lon"],
+    columns="aid_id",
+    values="haversine",
+    aggfunc=np.min,
 ).reset_index()
 
 agg_df.to_csv("data/design.csv", index=False)

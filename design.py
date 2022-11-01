b80bf7c99f0d532d93af7a2aa5778378812bbc9a
edkrueger <edkrueger@gmail.com>
2022-11-01 18:04:04 +0000

    Add counts to design matrix.

--- design.py
+++ design.py
@@ -3,11 +3,31 @@ import numpy as np
 
 df = pd.read_csv("data/distance.csv")
 
-agg_df = df.pivot_table(
+min_dist_df = df.pivot_table(
     index=["poll_id", "poll_lat", "poll_lon"],
     columns="aid_id",
     values="haversine",
     aggfunc=np.min,
-).reset_index()
+).rename(columns=lambda col: f"{col}_min_dist")
 
-agg_df.to_csv("data/design.csv", index=False)
+count_df = (
+    df.loc[lambda df_: df_["haversine"] <= 60]
+    .pivot_table(
+        index=["poll_id", "poll_lat", "poll_lon"],
+        columns="aid_id",
+        values="haversine",
+        aggfunc=len,
+    )
+    .rename(columns=lambda col: f"{col}_count")
+    # polling locations are generally not close enough to all aid locations
+    .fillna(0)
+)
+
+
+agg_df = (
+    pd.concat([min_dist_df, count_df], axis="columns")
+    # some polling stations are not close enough to any aid locations
+    .fillna(0)
+)
+
+agg_df.to_csv("data/design.csv")
